322e99322f3c23f8575267b25a973359e77a8534
mvanmeurs <masonvanmeurs7@gmail.com>
2019-11-18 18:36:43 -0500

    Adds a toggle for Dark Mode in Settings

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
 
   /// Authenticator with information about authenticated user.
   final BaseAuth auth = new Auth();
-  bool darkMode = false;
+  bool darkMode = true;
 
   /// Gets the name of the Login route.
   static String getLoginRouteName() {

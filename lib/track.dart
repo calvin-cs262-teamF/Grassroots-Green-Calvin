bc885346098ec75425d74d2368c11b0bd0fcedb3
tnm6 <tnm6@students.calvin.edu>
2019-11-18 19:23:03 -0500

    Track classes ready to go

--- lib/track.dart
+++ lib/track.dart
@@ -1,7 +1,51 @@
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:grassroots_green/auth.dart';
 
-// TODO: Refactor the "TRACK" page back into this file
+class Track extends StatelessWidget {
+
+  /// Routename for Navigation
+  static const String routeName = "/track";
+
+  /// Authenticator with user information.
+  final BaseAuth auth;
+
+  /// Constructor for Track.
+  Track({this.auth});
+
+  /// Builds the Track page
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TrackPage(auth: auth),
+    );
+  }
+}
+
+class TrackPage extends StatefulWidget {
+  
+  final BaseAuth auth;
+
+  TrackPage({Key key, this.auth}) : super(key: key);
+
+  @override
+  TrackPageState createState() => TrackPageState(auth: auth);
+}
+
+class TrackPageState extends State<TrackPage> {
+
+  final BaseAuth auth;
+
+  TrackPageState({this.auth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
 
 class MealsByDateChart extends StatelessWidget {
   final List<charts.Series> seriesList;

b272a15306e5c1b6264834b45ba2fffdd5584406
Elizabeth Koning <koningelizabeth@gmail.com>
2019-11-25 18:54:24 -0500

    Version of working group list for compete

--- lib/compete/groupList.dart
+++ lib/compete/groupList.dart
@@ -44,7 +44,7 @@ class GroupListStatefulWidgetState extends State<GroupListStatefulWidget> {
 
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('info').snapshots(),
+      stream: Firestore.instance.collection('groups').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
         if (snapshot.hasError) return ListView(shrinkWrap: true);
@@ -71,10 +71,9 @@ class GroupListStatefulWidgetState extends State<GroupListStatefulWidget> {
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey),
             borderRadius: BorderRadius.circular(5.0),
-            color: Theme.of(context).accentColor,
           ),
           child: ListTile(
-            title: Text(record.name, style: Theme.of(context).textTheme.button),
+            title: Text(record.name),
             onTap: () { _showGroupListPage(record); },
           )
       ),
@@ -88,14 +87,17 @@ class GroupListStatefulWidgetState extends State<GroupListStatefulWidget> {
 
 class GroupDoc {
   final String name;
-  final String admin;
+  final DocumentReference admin;
+  final CollectionReference members;
   final DocumentReference reference;
 
   GroupDoc.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['name'] != null),
         assert(map['admin'] != null),
+        assert(reference.collection('members').reference() != null),
         name = map['name'],
-        admin = map['admin'];
+        admin = map['admin'],
+        members = reference.collection('members').reference();
 
   GroupDoc.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
@@ -148,12 +150,44 @@ class GroupListSubPageStatefulWidgetState extends State<GroupListSubPageStateful
           child: ListView(
             shrinkWrap: true,
             children: <Widget>[
-              // TODO: use Theme data instead of hard-coded sizes
-              Text(doc.name, style: TextStyle(fontSize: 18, color: Theme.of(context).buttonColor, fontWeight: FontWeight.bold)),
+              Text(doc.name, style: Theme.of(context).textTheme.title),
+          StreamBuilder<QuerySnapshot>(
+            stream: doc.members.snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return LinearProgressIndicator();
+              if (snapshot.hasError) return ListView(shrinkWrap: true);
+              return _buildList(context, snapshot.data.documents);
+            },
+          ),
             ],
           )
-//          child: _getSources(doc.sources),
       ),
     );
   }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      shrinkWrap: true,
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    return Padding(
+      key: ValueKey(data['ref']),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text(data['name']),
+//            onTap: () { _showGroupListPage(record); },
+          )
+      ),
+    );
+  }
+
 }
